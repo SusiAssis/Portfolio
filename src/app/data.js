1523a42d5c244898ec344341f33d2aea4be153dc
Susilane Assis <118978790+SusiAssis@users.noreply.github.com>
2023-09-18 10:43:17 -0300

    fix: atualizar fotos

--- src/app/data.js
+++ src/app/data.js
@@ -1,17 +1,17 @@
 const projectData = [
   {
     title: "Nu Kenzie",
-    thumbnail: "/../public/projects imagens/NuKenzie.jpg", // Substitua pelo caminho da miniatura do Projeto 1
+    thumbnail: "/projects imagens/NuKenzie.jpg", // Substitua pelo caminho da miniatura do Projeto 1
     link: "https://react-entrega-s1-template-nu-kenzie-susi-assis-theta.vercel.app/",
   },
   {
     title: "Kenzie Hub",
-    thumbnail: "/../public/projects imagens/KenzieHub.jpg", // Substitua pelo caminho da miniatura do Projeto 2
+    thumbnail: "/projects imagens/KenzieHub.jpg", // Substitua pelo caminho da miniatura do Projeto 2
     link: "https://react-entrega-kenzie-hub-susi-assis.vercel.app/",
   },
   {
     title: "Burguer Kenzie",
-    thumbnail: "/../public/projects imagens/BurguerKenzie.jpg", // Substitua pelo caminho da miniatura do Projeto 2
+    thumbnail: "/projects imagens/BurguerKenzie.jpg", // Substitua pelo caminho da miniatura do Projeto 2
     link: "https://kenzie-burguer-v2-template-susilane-a.vercel.app/",
   },
 ];
